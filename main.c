65ef5d59da10bd9a5e841ec5b84e1754a40b1854
karnVen <onemonth06@gmail.com>
2026-02-14 21:39:24 +0530

    added lsh_split_line and lsh_launch

--- main.c
+++ main.c
@@ -2,6 +2,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
+#define LSH_RL_BUFSIZE 1024
+
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+
+
 int main (int argc, char **argv)
 {
   lsh_loop();
@@ -25,7 +32,7 @@ void lsh_loop(void)
   } while (status);
 }
 
-// #define LSH_RL_BUFSIZE 1024
+#define LSH_RL_BUFSIZE 1024
 // char *lsh_read_line(void)
 // {
 //   int bufsize = LSH_RL_BUFSIZE;
@@ -82,3 +89,38 @@ char *lsh_read_line(void)
 
   return line;
 }
+
+
+
+char **lsh_split_line(char *line)
+{
+  int bufsize = LSH_TOK_BUFSIZE, position = 0;
+  char **tokens = malloc(bufsize * sizeof(char*));
+  char *token;
+
+  if (!tokens) {
+    fprintf(stderr, "lsh: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  token = strtok(line, LSH_TOK_DELIM);
+  while (token != NULL) {
+    tokens[position] = token;
+    position++;
+
+    if (position >= bufsize) {
+      bufsize += LSH_TOK_BUFSIZE;
+      tokens = realloc(tokens, bufsize * sizeof(char*));
+      if (!tokens) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    token = strtok(NULL, LSH_TOK_DELIM);
+  }
+  tokens[position] = NULL;
+  return tokens;
+}
+
+
